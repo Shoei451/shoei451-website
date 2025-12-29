46f30c46da508a9ebd14f08d1d7a9c4aae5117d2
Shoei451 <okamotoshoei451@gmail.com>
2025-12-29 13:47:02 +0900

    Dec. 29th ver1

--- history/rekisou-forms/forms-data.js
+++ history/rekisou-forms/forms-data.js
@@ -1,13 +1,31 @@
 const formData = {
-  modern: [
-    { title: "産業革命チェックテスト", url: "https://forms.gle/xxxxxxxx" },
-    { title: "市民革命確認問題", url: "https://forms.gle/yyyyyyyy" }
+  folder1: [
+    { title: "【ヨーロッパの国民統合 1】", url: "https://docs.google.com/forms/d/e/1FAIpQLSezS7MgH2jToIxTNA3FPhkPotDI_VtJ2tW9ZGkv81gmSzlKqg/viewform?usp=dialog" },
+    { title: "【ヨーロッパの国民統合 2】", url: "https://docs.google.com/forms/d/e/1FAIpQLSdAN1z76NoHKCmCLwh_FZZHyl3_qzvE35bR74w8NNbDhvFAPw/viewform?usp=dialog" },
+    { title: "【19世紀のアメリカとアジア 1】", url: "https://docs.google.com/forms/d/e/1FAIpQLSeYLVfEqLsaEsOpr-PSbWkKrMjpCk5Le3j3osj0GF-IPLOo7Q/viewform?usp=dialog" },
+    { title: "【19世紀のアメリカとアジア 2】", url: "https://docs.google.com/forms/d/e/1FAIpQLSfYnkKHwkqYbcLU6ZBKs9GvYp0WYLo4iYQLpz7tjGD_0Q2nPw/viewform?usp=dialog" },
   ],
-  imperialism: [
-    { title: "帝国主義理解テスト", url: "https://forms.gle/zzzzzzzz" }
+  folder2: [
+    { title: "新政府の発足・殖産興業", url: "https://forms.gle/vNfgck5wybFpjYJi7" },
+    { title: "明治初期の対外関係", url: "https://forms.gle/rx613i1B3cCjM2BQ8" },
+    { title: "自由民権運動", url: "https://forms.gle/nSVKFMZtVucnnciW9" },
+    { title: "松方財政", url: "https://forms.gle/GvnTU4agzJt7DT878" },
+    { title: "立憲体制の成立", url: "https://forms.gle/FTCCtdduboQXghRm9" },
   ],
-  war: [
-    { title: "第一次世界大戦 確認フォーム", url: "https://forms.gle/aaaaaaaa" },
-    { title: "国際連盟 理解度チェック", url: "https://forms.gle/bbbbbbbb" }
+  folder3: [
+    { title: "第二次産業革命", url: "https://forms.gle/8XovPD568f1iAmmb9" },
+    { title: "帝国主義総集編", url: "https://forms.gle/p6c3Hc2LgFncnjjr8" },
+    { title: "アフリカ分割", url: "https://forms.gle/yjsjsZsDHchNXDA79" },
+  ],
+  folder4: [
+    { title: "【日清戦争と日本の産業革命】", url: "https://docs.google.com/forms/d/e/1FAIpQLSfDgoazLawsb0Y3bynUihRCQknhb1PW5nF9nR8EQ84_sQVexw/viewform?usp=header" },
+    { title: "【日露戦争とアジア 1】", url: "https://docs.google.com/forms/d/e/1FAIpQLScOQO0pJXj4Fo13NTIVeiRfLHutZYmvZXfJAnAuTkfF5uZorQ/viewform?usp=dialog" },
+    { title: "【日露戦争とアジア 2】", url: "https://docs.google.com/forms/d/e/1FAIpQLSf-JbJbZHg7pXquRb8vvbrYjtkje8JAlX-ygQu47_6SQBaE9g/viewform?usp=sf_link" },
+  ],
+  folder5: [
+    { title: "地図問題編", url: "https://docs.google.com/forms/d/e/1FAIpQLSeP3kteUFDAXoyqaua8dBRL6w2CdAGjpsgNSAKjTdfFJCo6qw/viewform?usp=dialog" },
+    { title: "番外編 授業プリントダイジェスト", url: "https://forms.gle/VN1VKxVd4TsX4ikNA" },
+    { title: "番外編 公共 【古代ギリシアの哲学者たち】", url: "https://docs.google.com/forms/d/e/1FAIpQLSfVnnUuKiGdUIX9UWQpT0FNIju6BUiym5YyJstsSbUo2cDDoA/viewform?usp=sf_link" },
+    { title: "番外編 公共 【28 国民所得と経済成長】~【30 金融の役割】", url: "https://docs.google.com/forms/d/e/1FAIpQLSfCzjhRJ9W0AnqZMxZ15YrAv2hBG5kCDSr4jHCld1wf7c8Fog/viewform?usp=sf_link" },
   ]
 };
