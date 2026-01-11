12279c3145a5689de67b0893f4245a7a5b2fcda9
Shoei451 <okamotoshoei451@gmail.com>
2026-01-11 16:00:40 +0900

    list1 update

--- home_economics/list1.js
+++ home_economics/list1.js
@@ -1,3 +1,9 @@
 const sectionItems1 = [
-  
+  {
+        icon: "🍽️",  
+        title: "食品・衛生分野 まとめノート",
+        titleEN: "Food & Hygiene Summary Notes",
+        description: "食品・衛生分野のまとめノートを配布します。",
+        link: "https://lateral-level-5c0.notion.site/2e55c4a6006b80b49944ea6db4638044?source=copy_link"
+    },
 ];
