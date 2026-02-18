1732650f57202eee36f3c8ca44f199c7ac59e3c1
Shoei451 <okamotoshoei451@gmail.com>
2026-02-18 14:58:14 +0900

    geo quiz

---
+++ others/list2.js
@@ -0,0 +1,12 @@
+const sectionItems2 = [
+    {
+        icon: "🦴",  
+        iconType: "",
+        title: "地質年代クイズ",
+        titleEN: "Geological Eras & Life",
+        description: "生物の進化と地質年代の変遷を、インタラクティブな年表とクイズで学ぶ",
+        link: "geology_quiz",
+        target: "_blank"
+    },
+    
+];
