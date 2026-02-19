f34dc502ff19e87e77dec88ce1ee09d1be2aa6b1
Shoei451 <okamotoshoei451@gmail.com>
2026-02-19 20:24:48 +0900

    sub-index applied

--- home_economics/list3.js
+++
@@ -1,9 +0,0 @@
-const sectionItems3 = [
-    {
-        icon: "📝",  
-        title: "2025年度 家庭基礎 定期試験過去問",
-        titleEN: "2025 Home Economics Basics Past Exams",
-        description: "2025年度の家庭基礎の定期試験過去問を掲載しています。試験対策にご活用ください。<br>※学校内限定公開",
-        link: "past-exams.html"
-    },
-];
