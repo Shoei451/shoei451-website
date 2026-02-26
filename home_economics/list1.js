ba4bcb9fb1b28db8abc1ab240b8aac83122c17c9
Shoei451 <okamotoshoei451@gmail.com>
2026-02-26 21:43:33 +0900

    home economixs

--- home_economics/list1.js
+++ home_economics/list1.js
@@ -6,4 +6,11 @@ const sectionItems1 = [
         description: "食品・衛生分野のまとめノートを配布します。",
         link: "https://shoei451.super.site/%e5%ae%b6%e5%ba%ad%e5%9f%ba%e7%a4%8e-%e9%a3%9f%e5%93%81%e8%a1%9b%e7%94%9f%e5%88%86%e9%87%8e%e3%81%be%e3%81%a8%e3%82%81%e3%83%8e%e3%83%bc%e3%83%88"
     },
+    {
+        icon: "📚",
+        title: "社会保障制度+民法 フラッシュカード",
+        titleEN: "Social Security System + Civil Law Flashcards",
+        description: "社会保障制度と民法のフラッシュカードを配布します。",
+        link: "frashcards.html"
+    },
 ];
