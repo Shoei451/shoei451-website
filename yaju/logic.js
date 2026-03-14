aa4cd14b97b395d7831bc519e7ee83f59e2d3248
Shoei451 <okamotoshoei451@gmail.com>
2026-03-14 18:47:58 +0900

    パスワード管理を円滑化

--- yaju/logic.js
+++ yaju/logic.js
@@ -79,28 +79,6 @@ startBtn.addEventListener("click", startTimer);
 stopBtn.addEventListener("click", stopTimer);
 resetBtn.addEventListener("click", resetTimer);
 
-// ===================== PASSWORDS =====================
-const passwordMap = {
-  "0760": "success.html",
-  "1420": "success.html",
-  "1831": "success.html"
-};
-
-const specialCodes = {
-  "0810": {
-    audio: "audio/hikakin.mp3",
-    img:   "images/hikakin.png",
-    title: "!!! SPECIAL ALERT !!!",
-    sub:   "なにを四天王"
-  },
-  "114514": {
-    audio: "audio/YAJU&U_8bit_cut.mp3",
-    img:   "images/cozy.jpg",
-    title: "!!! SPECIAL ALERT !!!",
-    sub:   "す こ こ い"
-  }
-};
-
 // ===================== LIVES =====================
 function updateLives() {
   lifeBlocks.forEach((block, i) => {
