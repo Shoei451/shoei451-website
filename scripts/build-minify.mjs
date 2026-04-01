bc4bebcbe04fea1a3128fd9e30126c86399cdd7f
Shoei451 <okamotoshoei451@gmail.com>
2026-04-01 17:44:09 +0900

    minify html

---
+++ scripts/build-minify.mjs
@@ -0,0 +1,27 @@
+// scripts/build-minify.mjs
+import { globSync } from "glob";
+import { execSync } from "node:child_process";
+
+const js = globSync("dist/**/*.js", {
+  ignore: ["dist/node_modules/**"],
+  absolute: true,
+});
+
+const css = globSync("dist/**/*.css", {
+  ignore: ["dist/node_modules/**"],
+  absolute: true,
+});
+
+if (js.length) {
+  execSync(
+    `node_modules/.bin/esbuild ${js.join(" ")} --minify --allow-overwrite`,
+    { stdio: "inherit" },
+  );
+}
+
+if (css.length) {
+  execSync(
+    `node_modules/.bin/esbuild ${css.join(" ")} --minify --allow-overwrite`,
+    { stdio: "inherit" },
+  );
+}
